2481611a7120e7b3bb93a2a23936cba72d0b266c
ardian56 <56733229+ardian56@users.noreply.github.com>
2025-03-12 08:41:53 +0700

    connect mongodb

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <div className="w-64 h-screen bg-red-900 text-white p-5">
+    <div className="w-64 h-screen bg-orange-900 text-white p-5">
       <h2 className="text-lg font-semibold">Admin Panel</h2>
       <ul className="mt-4">
         <li className="py-2 px-3 hover:bg-blue-700 rounded">
